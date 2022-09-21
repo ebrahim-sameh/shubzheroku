519616ad794742c476348b2e26a6a15f8a6a72fc
ebrahim <ebrahimsameh16@gmail.com>
2022-09-21 03:59:03 +0200

    changing django respect of the order of apps in settings

--- intropage/urls.py
+++ intropage/urls.py
@@ -18,7 +18,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.mainpage, name='home' ),
+
     path('studio', views.audio, name='audio'),
+
     # path('scrape/', views.scrape, name='scrape')
 
 ]
